373d0c83cef7a2fc61f2cf823548840311f214af
Gleb Nikonorov <gleb.i.nikonorov@gmail.com>
2018-10-21 12:54:15 -0400

    Implement logging to STDOUT and STDERR for predefined log levels

--- bog.sh
+++ bog.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 #
 # Logging framework for BASH scripts. Source this script to use BOG.
+#
+# By default, output will go to STDOUT and STDERR streams as follows:
+#   debug, info and warning will go to STDOUT
+#   error and fatal will go to STDERR
 
 function bog_log_debug() {
-    return
+    local log_message="$1"
+    local output_string
+
+    output_string="DEBUG: $(date) - $log_message"
+    echo -e "$output_string"
 }
 
 function bog_log_info() {
-    return
+    local log_message="$1"
+    local output_string
+
+    output_string="INFO: $(date) - $log_message"
+    echo -e "$output_string"
 }
 
 function bog_log_warning() {
-    return
+    local log_message="$1"
+    local output_string
+
+    output_string="WARNING: $(date) - $log_message"
+    echo -e "$output_string"
 }
 
 function bog_log_error() {
-    return
+    local log_message="$1"
+    local output_string
+
+    output_string="ERROR: $(date) - $log_message"
+    >&2 echo -e "$output_string"
 }
 
 function bog_log_fatal() {
-    return
+    local log_message="$1"
+    local output_string
+
+    output_string="FATAL: $(date) - $log_message"
+    >&2 echo -e "$output_string"
 }
