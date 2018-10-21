9cc349a863a51e08bd400af55a4beb584d776fbb
Gleb Nikonorov <gleb.i.nikonorov@gmail.com>
2018-10-21 12:40:20 -0400

    Add in skeleton

---
+++ bog.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Logging framework for BASH scripts. Source this script to use BOG.
+
+function bog_log_debug() {
+    return
+}
+
+function bog_log_info() {
+    return
+}
+
+function bog_log_warning() {
+    return
+}
+
+function bog_log_error() {
+    return
+}
+
+function bog_log_fatal() {
+    return
+}
